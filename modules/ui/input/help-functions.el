4d6824f5ba1ffc177295749dfb9fce1700760da9
Jeff Parsons <parsoj@gmail.com>
2020-01-22 19:04:22 -0800

    add popup rule for help buffers; run auto-formatter

--- modules/ui/input/help-functions.el
+++ modules/ui/input/help-functions.el
@@ -2,40 +2,44 @@
 (with-eval-after-load '+hydra
   (with-eval-after-load 'ivy
 
-  (setq hydra-help-actions--title (s-concat (all-the-icons-faicon "question" :v-adjust .025) " Help Actions" )) 
+    (setq hydra-help-actions--title (s-concat (all-the-icons-faicon "question" :v-adjust .025) " Help Actions" )) 
 
-  (defvar +doc-at-point-func nil)
+    (defvar +doc-at-point-func nil)
 
-  (pretty-hydra-define hydra-help-actions
-                        (:color teal :title hydra-help-actions--title)
-                        (
-			 "Describe"
-                         (
-			  ("h" (lambda () (interactive) (funcall +doc-at-point-func)))
-			  ("a" counsel-apropos "apropos")
-                          ("f" describe-function "function") ;; TODO move to function-at-point at some point
-                          ("v" describe-variable "variable")
-                          ("m" describe-mode "mode")
-                          ("k" describe-key "key")
-			  )
-			)
-			)
-
-
-  (general-define-key
-   :states '(normal visual emacs movement treemacs)
-   :prefix "SPC"
-   :non-normal-prefix "M-SPC"
-   "h" 'hydra-help-actions/body
-   )
+    (pretty-hydra-define hydra-help-actions
+      (:color teal :title hydra-help-actions--title)
+      (
+       "Describe"
+       (
+	("h" (lambda () (interactive) (funcall +doc-at-point-func)))
+	("a" counsel-apropos "apropos")
+        ("f" describe-function "function") ;; TODO move to function-at-point at some point
+        ("v" describe-variable "variable")
+        ("m" describe-mode "mode")
+        ("k" describe-key "key")
+	)
+       )
+      )
 
 
-;;  (add-to-list 'display-buffer-alist
-;;	     `("*Help*"
-;;	       ,(lambda (buffer alist) (display-buffer-in-direction buffer (cons '(direction . right) alist)))
-;;	       (window-width . 0.33)
-;;	       ))
+    (general-define-key
+     :states '(normal visual emacs movement treemacs)
+     :prefix "SPC"
+     :non-normal-prefix "M-SPC"
+     "h" 'hydra-help-actions/body
+     )
 
-)
 
-)
+
+    (set-popup-rule!
+      "^\\*Help*" :side 'right :slot -1 :size 0.35 :select t) 
+
+    ;;  (add-to-list 'display-buffer-alist
+    ;;	     `("*Help*"
+    ;;	       ,(lambda (buffer alist) (display-buffer-in-direction buffer (cons '(direction . right) alist)))
+    ;;	       (window-width . 0.33)
+    ;;	       ))
+
+    )
+
+  )
