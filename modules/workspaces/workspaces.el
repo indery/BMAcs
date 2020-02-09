98ed28d33e8c0385132fd580f7f1290105cdae6d
Jeff Parsons <jeffparsons@Jeffs-MacBook-Pro.local>
2020-02-09 11:05:42 -0800

    add some stubs and organization to workspaces file

--- modules/workspaces/workspaces.el
+++ modules/workspaces/workspaces.el
@@ -1,12 +1,45 @@
-;; TODO: Workspace create
 
-;; TODO: Workspace switch
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; finding workspaces
+(defvar +workspaces-directories)
 
-;; TODO: Workspace jump to settings
+(defun +workspaces-list-all-workspaces ()
 
-;; TODO: Workspace jump to notes
+  )
 
-;; TODO: Workspace jump to scratch file
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; workspace CRUD actions
+(defun +workspaces-create-workspace ()
+  ;; TODO: Workspace create
+  (interactive)
+  (progn
+    (treemacs-create-workspace)
+    ;;TODO create workspace directory
+    ;;TODO populate workspace with repos...
+    )
+
+  )
+
+(defun +workspaces-switch-workspace ()
+  (interactive)
+  ;; TODO: Workspace switch
+  ;;; change projectile 
+  )
+
+;; TODO: Workspace delete
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; workspace manage projects 
+
+;; TODO: Workspace add project
+
+;; TODO: Workspace delete project
+
+;; TODO: Workspace sync project
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; save restore, and manage workspace state
 
 ;; TODO: Workspace save buffers
 
@@ -16,14 +49,16 @@
 
 ;; TODO: Workspace load buffers
 
-;; TODO: Workspace delete
 
-;; TODO: Workspace capture
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; workspace-scoped shortcuts/actions
 
-;; TODO: Workspace add project
+;; TODO: Workspace jump to settings
 
-;; TODO: Workspace delete project
+;; TODO: Workspace jump to notes
 
-;; TODO: Workspace sync project
+;; TODO: Workspace jump to scratch file
 
 ;; TODO: Workspace run commands (custom keybindings)
+
+;; TODO: Workspace org-capture
