23a0b0cb0acf22a107f69f31d72ffd877d9bcbdc
Jeff Parsons <parsoj@gmail.com>
2020-02-21 19:23:40 -0800

    add fix for company mode keybindings

--- modules/editor/coding_features/company.el
+++ modules/editor/coding_features/company.el
@@ -2,19 +2,27 @@
 
   :init
   (setq company-idle-delay .6)   
+
+  :config
+  
+  (with-eval-after-load 'doom/keymapper
+    ;; evil tries to over-write this - so RN we will just run this after evil loads
+    (with-eval-after-load '+evil
+      (map!
+       :map 'company-active-map
+       "TAB" 'company-complete-selection
+       "<tab>" 'company-complete-selection
+       "RET" 'company-complete-selection
+       ))
+
+    )
+
   )
 
-(with-eval-after-load 'doom/keymapper
-  (map!
-   :map'company-active-map
-   "TAB" 'company-complete-selection
-   "<tab>" 'company-complete-selection
-   "RET" 'company-complete-selection
-   )
 
-  ) 
+
 
 (use-package company-quickhelp
   :after company
   :config
-  (company-quickhelp-mode))
+  (company-quickhelp-mode)) 
