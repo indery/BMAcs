3aed1ee4b4533007054f051a60e1f791951631ff
Jeff Parsons <parsoj@gmail.com>
2020-02-28 17:50:07 -0800

    minimap config tweaks

--- modules/ui/widgets/minimap/minimap.el
+++ modules/ui/widgets/minimap/minimap.el
@@ -3,8 +3,12 @@
 
   :config
   (require 'sublimity-map)
-  (sublimity-map-set-delay 1)
+  (sublimity-map-set-delay 0.5)
   (setq sublimity-map-active-region 'region)
+
+  (setq sublimity-map-size 25)
+
+
 
   )
 
