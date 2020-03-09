a542bb7b8e93e204a8be93f2fb43475954139c41
Jeff Parsons <parsoj@gmail.com>
2020-03-08 17:19:07 -0700

    fix sourcekit-lsp settings to work with Xcode 11.4 beta

--- modules/editor/languages/swift.el
+++ modules/editor/languages/swift.el
@@ -7,9 +7,9 @@
   :after lsp-mode
   :init
   (setq
-   sourcekit-toolchain-path "/Library/Developer/Toolchains/swift-5.1-DEVELOPMENT-SNAPSHOT-2019-08-31-a.xctoolchain"
+   sourcekit-toolchain-path "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain"
    sourcekit-exec-files (concat sourcekit-toolchain-path "/usr/bin")
-   lsp-sourcekit-executable (concat sourcekit-exec-files "sourcekit-lsp")
+   lsp-sourcekit-executable (concat sourcekit-exec-files "/sourcekit-lsp")
    )
 
   (setenv "SOURCEKIT_TOOLCHAIN_PATH" sourcekit-toolchain-path) 
