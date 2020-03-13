0e439074eac39d56de6995b04493a5e467816ae9
Jeff Parsons <parsoj@gmail.com>
2020-03-12 18:49:30 -0700

    finish inf-mongo impl

--- modules/external-apps/development/mongo.el
+++ modules/external-apps/development/mongo.el
@@ -1,3 +1,36 @@
+
 (use-package inf-mongo
   :straight (:host github :repo "endofunky/inf-mongo")
   )
+
+
+(defun build-mongo-connect-command (cluster-url db-name username password)
+
+  (format 
+   "/usr/local/bin/mongo mongodb+srv://%s/%s --username %s --password %s"
+   cluster-url
+   db-name
+   username
+   password
+   )
+
+  )
+
+
+(defun +mongo-connect ()
+  (interactive)
+  (let (
+	(cluster-data (cdr (assoc (completing-read "Which Mongo Cluster?" mongo-clusters) mongo-clusters)))
+	)
+
+
+    (inf-mongo (build-mongo-connect-command
+		(car cluster-data)
+		""
+		(+vault-get-secret (nth 1 cluster-data))
+		(+vault-get-secret (nth 2 cluster-data))
+		))
+    ))
+
+
+
