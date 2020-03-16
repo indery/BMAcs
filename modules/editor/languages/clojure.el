be6b28498009ffb4ff5e64d9627aa995c08c6635
Jeff Parsons <parsoj@gmail.com>
2020-03-15 18:47:09 -0700

    add initial clojure config

--- modules/editor/languages/clojure.el
+++ modules/editor/languages/clojure.el
@@ -2,8 +2,17 @@
 
 
 (use-package cider
-;;  :ensure-system-package (
-;;			  "adoptopenjdk8"
-;;			  "clojure")
+  ;;  :ensure-system-package (
+  ;;			  "adoptopenjdk8"
+  ;;			  "clojure")
+
+  :hook ( (clojure-mode . rainbow-delimiters-mode)
+	  (clojure-mode . cider-mode)
+	  (clojure-mode . company-mode)
+	  )
+  :init
+  (setq cider-overlays-use-font-lock t)
+  (setq cider-use-overlays t)
+
 
   )
