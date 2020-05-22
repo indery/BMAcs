4768873d65edf8a5ae817df832b39b2e27a91859
Jeff Parsons <parsoj@gmail.com>
2020-05-21 20:45:44 -0700

    show some todos in magit

--- modules/tools/git/magit.el
+++ modules/tools/git/magit.el
@@ -20,5 +20,19 @@
   )
 
 
+(use-package magit-todos
+  :straight (magit-todos :host github :repo "alphapapa/magit-todos")
+  )
+
+(use-package magit-org-todos
+  :config
+  (magit-add-section-hook
+   'magit-status-sections-hook
+   'magit-org-todos-insert-org-todos
+   nil
+   'append
+   )
+  )   
+
 (set-popup-rule!
   "^magit:" :side 'right :slot -1 :size 0.40 :select t) 
