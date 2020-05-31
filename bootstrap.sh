9b56d4d73677c9d91f965e163c04c22d0fd18822
Jeff Parsons <parsoj@gmail.com>
2020-05-31 11:04:13 -0700

    update install script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 brew tap d12frosted/emacs-plus
-#brew install emacs-plus --HEAD --with-jansson --with-no-frame-refocus --without-spacemacs-icon --with-dbus
-brew install emacs-plus --with-no-frame-refocus --without-spacemacs-icon --with-modern-icon
+brew install emacs-plus --HEAD --with-jansson --with-no-frame-refocus --with-modern-icon --with-dbus
+#brew install emacs-plus --with-no-frame-refocus --without-spacemacs-icon --with-modern-icon
 
