b576fd71ac096e4ddbbc12c2163b25fe96215bd8
Jeff Parsons <parsoj@gmail.com>
2020-06-09 20:59:53 -0700

    org pretty tables

--- modules/editor/languages/org/appearance.el
+++ modules/editor/languages/org/appearance.el
@@ -2,3 +2,8 @@
 (use-package org-bullets
   :hook (org-mode . org-bullets-mode)
   )
+
+(use-package org-pretty-table
+  :straight ( :host github :repo "fuco1/org-pretty-table")
+  :hook (org-mode . org-pretty-table-mode)
+  )
