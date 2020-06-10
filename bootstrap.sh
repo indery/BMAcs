f4557475c2cfcac77943b2ebfccab2322336c9a7
Jeff Parsons <parsoj@gmail.com>
2020-06-10 07:07:02 -0700

    update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-brew tap d12frosted/emacs-plus
-brew install emacs-plus --HEAD --with-jansson --with-no-frame-refocus --with-modern-icon-papirus --with-debug --with-no-titlebar --with-xwidgets
+brew tap parsoj/emacs-plus
+brew install emacs-plus --HEAD --with-jansson --with-no-frame-refocus --with-modern-icon-papirus --with-debug --with-xwidgets
 #brew install emacs-plus --with-no-frame-refocus --without-spacemacs-icon --with-modern-icon
 
